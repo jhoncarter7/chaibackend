c43d3fe57bedc3483a1b01d6cf4c31687588d942
jhoncarter7 <rainikhil044@gmail.com>
2024-03-02 11:03:20 +0530

    Add comment, like, playlist, tweet, and video models

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema({
     owner:{
         type: Schema.Types.ObjectId,
@@ -35,5 +35,5 @@ const videoSchema = new Schema({
     },
 }, {timestamps: true})
 
-
+videoSchema.plugin(mongooseAggregatePaginate); //pagination plugin
 export const Video = mongoose.model("Video", videoSchema)
